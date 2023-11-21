01f894cb15b4630334fec8de1862995e6f95565b
ar3shoaib <ar.shoaib16@gmail.com>
2023-11-21 01:01:17 -0500

    Implemented Emotion Detection with DeepFace

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import cv2
+from deepface import DeepFace
 
 # Classifier gives (x,y), width and height
 face_classifier = cv2.CascadeClassifier()
@@ -13,11 +16,19 @@ while True:
     # RGB to grayscale then pass to classifier
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(frame_gray)
+
+    # Give deepface an image so that it returns an emotion
+    response = DeepFace.analyze(frame, actions=("emotion",), enforce_detection=False)
+    #print(response)
+
+
     # print(faces)
 
     for face in faces:
         # Create rectangle on frame
         x, y, w, h = face
+        # print(response[0]['dominant_emotion'])
+        cv2.putText(frame, text = response[0]['dominant_emotion'], org=(x,y), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(0, 255, 0))
         new_frame = cv2.rectangle(frame, (x, y), (x+w, y+h), color=(255, 0, 0), thickness=2)
 
 
